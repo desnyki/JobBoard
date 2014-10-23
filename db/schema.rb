a1472369ad6107f16a7f41bf187b1a014993e2b2
desnyki <desnyki@gmail.com>
2014-10-22 20:01:07 -0400

    sup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022002259) do
+ActiveRecord::Schema.define(version: 20141022230710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,18 @@ ActiveRecord::Schema.define(version: 20141022002259) do
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
